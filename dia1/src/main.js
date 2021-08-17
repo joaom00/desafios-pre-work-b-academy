7dfb326a720433bb4fe7d90ad187f49e1a0ce1bc
João Pedro <joaomljcanal@gmail.com>
2021-08-17 10:04:44 -0300

    day1/challenge 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,22 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]')
+const link = document.querySelector('[data-js="link"]')
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+  const isHidden = app.style.visibility === 'hidden'
+
+  if (isHidden) {
+    app.style.visibility = 'visible'
+    link.textContent = 'Esconder'
+  } else {
+    app.style.visibility = 'hidden'
+    link.textContent = 'Mostrar'
+  }
+})
